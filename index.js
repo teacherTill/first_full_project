676bf0fe84ac7bc793170866aafb5ab096517d07
badgiro <mulbashev98@gmail.com>
2026-04-16 10:01:24 +0300

    unfinished third screen

---
+++ index.js
@@ -0,0 +1,8 @@
+const cookie = document.querySelector('.cookie')
+
+document.querySelector('.cookie__reject').addEventListener('click', () => {
+  cookie.classList.add('hidden')
+})
+
+// cookie__reject
+// cookie__accept
