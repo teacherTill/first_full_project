7cb6b7116f0da7dcd1fdcdb620aac184d0e5466b
badgiro <mulbashev98@gmail.com>
2026-04-17 12:48:02 +0300

    reference of accordion

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ document.querySelector('.cookie__reject').addEventListener('click', () => {
   cookie.classList.add('hidden')
 })
 
-// cookie__reject
-// cookie__accept
+const accordion_one = document.getElementById('accordion_one')
+console.log(accordion_one)
+accordion_one.onclick = (e) => {
+  e.currentTarget.classList.toggle('accordion_active')
+}
